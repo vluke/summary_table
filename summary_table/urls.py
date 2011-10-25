94bf520bc4a24690ec1fd48f58f8e05f380e975f
ryan.braganza@gmail.com <ryan.braganza@gmail.com@1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e>
2011-10-25 07:00:36 +0000

    use server-side data processing/filtering (summary_table)
    still TODO: sorting
    
    
    git-svn-id: https://mytardis.googlecode.com/svn/apps/summary_table/trunk@2111 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- summary_table/urls.py
+++ summary_table/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns(
     'tardis.apps.summary_table.views',
     (r'^(?P<experiment_id>\d+)/$', 'index'),
+    (r'^(?P<experiment_id>\d+)/table/$', 'table'),
     )
 
--- summary_table/views.py
+++ summary_table/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.db.models import Q
 from django.http import HttpResponse
 from django.template import Context
 from django.shortcuts import render_to_response, redirect
@@ -9,6 +10,8 @@ from tardis.tardis_portal.auth import decorators as authz
 from tardis.tardis_portal.creativecommonshandler import CreativeCommonsHandler
 from tardis.tardis_portal.models import Experiment, DatafileParameter, ParameterName, Dataset_File
 from tardis.tardis_portal.shortcuts import render_response_index
+
+import json
 
 import logging
 logger = logging.getLogger(__name__)
@@ -24,16 +27,48 @@ def index(request, experiment_id):
     parameter_names = ParameterName.objects.filter(datafileparameter__parameterset__dataset_file__dataset__experiment=experiment).distinct()
     datafiles = Dataset_File.objects.filter(dataset__experiment=experiment)
 
-    grid = []
-    for df in datafiles:
+    c['parameter_names'] = parameter_names
+    c['experiment'] = experiment
+
+    return HttpResponse(render_response_index(request, url, c))
+
+@authz.experiment_access_required
+def table(request, experiment_id):
+    # http://datatables.net/usage/server-side
+
+    experiment = Experiment.objects.get(pk=experiment_id)
+
+    parameter_names = ParameterName.objects.filter(datafileparameter__parameterset__dataset_file__dataset__experiment=experiment).distinct()
+    datafiles = Dataset_File.objects.filter(dataset__experiment=experiment)
+
+    limit = int(request.GET['iDisplayLength'])
+    offset = int(request.GET['iDisplayStart'])
+
+    filter = request.GET['sSearch']
+
+    filtered_datafiles = _filter(datafiles, filter)
+
+    rows = []
+    for df in filtered_datafiles[offset:offset+limit]:
         row = []
         row.append(df.filename)
         for pn in parameter_names:
             params = DatafileParameter.objects.filter(parameterset__dataset_file=df, name=pn)
             row.append(','.join([param.get() for param in params]))
-        grid.append(row)
+        rows.append(row)
 
-    c['parameter_names'] = parameter_names
-    c['grid'] = grid
+    resp = {}
+    resp['sEcho'] = request.GET['sEcho']
+    resp['aaData'] = rows
+    resp['iTotalRecords'] = datafiles.count()
+    resp['iTotalDisplayRecords'] = filtered_datafiles.count()
+    return HttpResponse(json.dumps(resp), mimetype='application/json')
 
-    return HttpResponse(render_response_index(request, url, c))
+def _filter(datafile_queryset, filter):
+    if len(filter) < 1:
+        return datafile_queryset
+    else:
+        query = Q(filename__icontains=filter)
+        query |= Q(datafileparameterset__datafileparameter__string_value__icontains=filter)
+        query |= Q(datafileparameterset__datafileparameter__numerical_value__icontains=filter)
+        return datafile_queryset.filter(query).distinct()
