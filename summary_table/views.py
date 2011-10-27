442e365291913ef7715b2d355749a49117e5aa97
ryan.braganza@gmail.com <ryan.braganza@gmail.com@1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e>
2011-10-27 06:18:06 +0000

    improve remote performance (summary_table)
    git-svn-id: https://mytardis.googlecode.com/svn/apps/summary_table/trunk@2119 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- summary_table/views.py
+++ summary_table/views.py
@@ -27,7 +27,10 @@ def index(request, experiment_id):
     parameter_names = ParameterName.objects.filter(datafileparameter__parameterset__dataset_file__dataset__experiment=experiment).distinct()
     datafiles = Dataset_File.objects.filter(dataset__experiment=experiment)
 
-    c['parameter_names'] = parameter_names
+    descs = [{'mDataProp': 'filename', 'sTitle': 'filename'}]
+    for x in parameter_names:
+        descs.append({'mDataProp': str(x.id), 'sTitle': x.name})
+    c['joined'] = json.dumps(descs)
     c['experiment'] = experiment
 
     return HttpResponse(render_response_index(request, url, c))
@@ -35,11 +38,28 @@ def index(request, experiment_id):
 @authz.experiment_access_required
 def table(request, experiment_id):
     # http://datatables.net/usage/server-side
+    logger.debug(request.GET)
+    logger.debug(request.GET['sColumns'])
+    if not int(request.GET['iSortingCols']) == 1:
+        raise Exception('this should not happen')
+    sort_col_index = request.GET['iSortCol_0']
+    sort_col_name = request.GET['mDataProp_' + sort_col_index]
+    sort_desc = request.GET['sSortDir_0'] == 'desc'
 
     experiment = Experiment.objects.get(pk=experiment_id)
 
     parameter_names = ParameterName.objects.filter(datafileparameter__parameterset__dataset_file__dataset__experiment=experiment).distinct()
     datafiles = Dataset_File.objects.filter(dataset__experiment=experiment)
+
+    if sort_col_name == 'filename':
+        if sort_desc:
+            datafiles = datafiles.order_by('-filename')
+        else:
+            datafiles = datafiles.order_by('filename')
+        post_filter = False
+    else:
+        datafiles = datafiles.order_by('filename')
+        post_filter = True
 
     limit = int(request.GET['iDisplayLength'])
     offset = int(request.GET['iDisplayStart'])
@@ -48,17 +68,91 @@ def table(request, experiment_id):
 
     filtered_datafiles = _filter(datafiles, filter)
 
+
+    pndict = dict([[pn.id, pn] for pn in parameter_names])
+    pn_ids = [x for x in pndict.keys()]
+
+    dfs = [(x.id, x.filename) for x in filtered_datafiles[offset:offset+limit]]
+    df_ids = [x[0] for x in dfs]
+    dfdict = dict(dfs)
+
+    name_ids = [pn.id for pn in parameter_names]
+
+    dfps = DatafileParameter.objects.filter(parameterset__dataset_file__in=df_ids, name__in=name_ids).values('parameterset__dataset_file__id', 'name__id', 'numerical_value', 'datetime_value', 'string_value')
+
+    params_by_file = {}
+    for dfp in dfps:
+        f_id = dfp['parameterset__dataset_file__id']
+        n_id = dfp['name__id']
+        if f_id not in params_by_file:
+            params_by_file[f_id] = {}
+
+        dfps_by_name = params_by_file[f_id]
+
+        if n_id not in dfps_by_name:
+            dfps_by_name[n_id] = []
+
+        dfp_vals = {'datetime_value': dfp['datetime_value'], 'numerical_value': dfp['numerical_value'], 'string_value': dfp['string_value'] }
+        dfps_by_name[n_id].append(dfp)
+
     rows = []
-    for df in filtered_datafiles[offset:offset+limit]:
-        row = []
-        row.append(df.filename)
-        for pn in parameter_names:
-            params = DatafileParameter.objects.filter(parameterset__dataset_file=df, name=pn)
-            row.append(','.join([param.get() for param in params]))
+    for dft in dfs:
+        df_id = dft[0]
+        df_name = dft[1]
+
+        row = {}
+        row['filename'] = df_name
+        for pn_id in pn_ids:
+            row[str(pn_id)] = ''
+            if post_filter:
+                row['sortable'] = 0
+        for pn_id, params in params_by_file[df_id].items():
+            pn = pndict[pn_id]
+            if pn.isString() or pn.isLongString():
+                row[str(pn_id)] = ','.join([param['string_value'] for param in params])
+            elif pn.isNumeric():
+                row[str(pn_id)] = ','.join([str(param['numerical_value']) for param in params])
+            elif pn.isDateTime():
+                row[str(pn_id)] = ','.join([str(param['datetime_value']) for param in params])
+
+            if post_filter and sort_col_name == str(pn.id):
+                if len(params) > 0:
+                    if pn.isString() or pn.isLongString():
+                        row['sortable'] = sorted([x['string_value'] for x in params])[0]
+                    elif pn.isNumeric():
+                        row['sortable'] = sorted([x['numerical_value'] for x in params])[0]
+                    elif pn.isDateTime():
+                        row['sortable'] = sorted([x['datetime_value'] for x in params])[0]
+                    else:
+                        row['sortable'] = 0
         rows.append(row)
 
+#    for df in filtered_datafiles[offset:offset+limit]:
+#        row = {}
+#        row['filename'] = df.filename
+#        for pn in parameter_names:
+#            params = DatafileParameter.objects.filter(parameterset__dataset_file=df, name=pn)
+#            row[str(pn.id)] = ','.join([param.get() for param in params])
+#            if post_filter and sort_col_name == str(pn.id):
+#                if len(params) == 0:
+#                    row['sortable'] = 0
+#                else:
+#                    if pn.isString() or pn.isLongString():
+#                        row['sortable'] = sorted([x.string_value for x in params])[0]
+#                    elif pn.isNumeric():
+#                        row['sortable'] = sorted([x.numerical_value for x in params])[0]
+#                    elif pn.isDateTime():
+#                        row['sortable'] = sorted([x.datetime_value for x in params])[0]
+#                    else:
+#                        row['sortable'] = 0
+#        rows.append(row)
+    if post_filter:
+        rows = sorted(rows, key=lambda x: x['sortable'], reverse=sort_desc)
+        for row in rows:
+            del row['sortable']
+
     resp = {}
-    resp['sEcho'] = request.GET['sEcho']
+    resp['sEcho'] = int(request.GET['sEcho'])
     resp['aaData'] = rows
     resp['iTotalRecords'] = datafiles.count()
     resp['iTotalDisplayRecords'] = filtered_datafiles.count()
